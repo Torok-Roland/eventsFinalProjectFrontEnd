2e821a475acd59e0c0472a1dc1ee274c6a4d2e88
Torok Roland <torok.roland1998@gmail.com>
2023-08-04 15:44:52 +0300

    Creating the category-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { EventPageComponent } from './event-page/event-page.component';
 import {HttpClientModule} from "@angular/common/http";
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoryComponentComponent } from './category-component/category-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventPageComponent,
-    NotFoundPageComponent
+    NotFoundPageComponent,
+    HeaderComponent,
+    CategoryComponentComponent
   ],
   imports: [
     BrowserModule,
